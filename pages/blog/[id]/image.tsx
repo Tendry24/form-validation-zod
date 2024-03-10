3b472cdb314520b8ed2202dfd10681b2049f5e4c
Tendry24 <hei.tendry.2@gmail.com>
2024-03-10 22:13:01 +0300

    fix: update some posts

--- pages/blog/[id]/image.tsx
+++ pages/blog/[id]/image.tsx
@@ -4,9 +4,12 @@ import LeftSide from '../../../components/LeftSide';
 import PostImage from '../../../components/PostImage';
 
 const posts: Post[] = [
- { id: 1, title: 'Post 1', content: 'Contenu du post 1' },
- { id: 2, title: 'Post 2', content: 'Contenu du post 2' },
- // Ajoutez plus de posts ici
+  { id: 1, title: 'Post 1', content: 'Content 1' },
+  { id: 2, title: 'Post 2', content: 'Content 2' },
+  { id: 3, title: 'Post 3', content: 'Content 3' },
+  { id: 4, title: 'Post 4', content: 'Content 4' },
+  { id: 5, title: 'Post 5', content: 'Content 5' },
+  { id: 6, title: 'Post 6', content: 'Content 6' },
 ];
 
 export default function PostImagePage() {
